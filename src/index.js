98a0fbed3878d1c5541c4015ef7f3f4d84b56166
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-02-12 20:02:46 +0200

    Learned how to work with styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/*
+REACT COURSE
+
+In this course, we will learn react js by building TaskOPedia!
+  - Call Ben
+  - Go to walmart
+
+Happy Coding!
+*/
+
+function MainHeader() {
+  return <h1>REACT COURSE</h1>;
+}
+
+const subHeaderStyle={
+  color:"blueviolet",
+  backgroundColor:"lightgray"
+}
+function SubHeader() {
+  return <p style={subHeaderStyle}>This will be an exciting course.</p>;
+}
+function Header() {
+  return (
+    <div>
+      <MainHeader></MainHeader>
+      <SubHeader></SubHeader>
+    </div>
+  );
+}
+function MainBody() {
+  return (
+    <div>
+      <p>In this course, we will learn react js by building TaskOPedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <p style={{ color: "gray", backgroundColor: "black" }}>Happy Coding!</p>
+  );
+}
 root.render(
-  <React.StrictMode> 
-  </React.StrictMode>
+  <div>
+    <Header />
+    <MainBody />
+    <Footer />
+  </div>
 );
-
