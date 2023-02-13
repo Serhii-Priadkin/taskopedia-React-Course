097a8335981dedc9ddcbf6a03a547d6d3561a6cb
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-02-13 20:37:37 +0200

    Separeted functions

---
+++ src/MainBody.jsx
@@ -0,0 +1,41 @@
+import Student from "./Student";
+
+function MainBody() {
+  const whatWeWillLearn = "React JS";
+  const lectureCount = 3;
+  return (
+    <div style={{ minHeight: "70vh" }}>
+      <p>
+        In this course, we will learn {whatWeWillLearn} by building TaskOPedia!{" "}
+        <br />
+        Total Lecture - {lectureCount}
+      </p>
+      <ul>
+        <li>Basic Foundation</li>
+        <li>Functional and Class Components</li>
+      </ul>
+      {/* <div>
+          Enter Task : <input maxLength={5} readOnly={false} placeholder="Ben"></input>
+        </div> */}
+
+      <div className="container row">Students Enrolled</div>
+      <Student
+        experience={2}
+        name="Cris Pratt"
+        headshot="https://api.lorem.space/image/face?w=150&h=150"
+      />
+      <Student
+        experience={5}
+        name="Patric Stuard"
+        headshot="https://api.lorem.space/image/face?w=150&h=151"
+      />
+      <Student
+        experience={3}
+        name="Cris Evans"
+        headshot="https://api.lorem.space/image/face?w=150&h=152"
+      />
+    </div>
+  );
+}
+
+export default MainBody;
