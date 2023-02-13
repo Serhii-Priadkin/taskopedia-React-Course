867c497e0b55649491bc7db186108579dfffd2c2
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-02-13 19:01:35 +0200

    Learned how to pass props to components and use

--- src/index.js
+++ src/index.js
@@ -1,56 +1,44 @@
 import ReactDOM from "react-dom/client";
+import "./CSS/style.css";
+import Header from "./Header";
+import Student from "./Student";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-/*
-REACT COURSE
-
-In this course, we will learn react js by building TaskOPedia!
-  - Call Ben
-  - Go to walmart
-
-Happy Coding!
-*/
-
-function MainHeader() {
-  return <h1>REACT COURSE</h1>;
-}
-
-const subHeaderStyle={
-  color:"blueviolet",
-  backgroundColor:"lightgray"
-}
-function SubHeader() {
-  return <p style={subHeaderStyle}>This will be an exciting course.</p>;
-}
-function Header() {
-  return (
-    <div>
-      <MainHeader></MainHeader>
-      <SubHeader></SubHeader>
-    </div>
-  );
-}
 function MainBody() {
+  const whatWeWillLearn = "React JS";
+  const lectureCount = 3;
   return (
     <div>
-      <p>In this course, we will learn react js by building TaskOPedia!</p>
+      <p>
+        In this course, we will learn {whatWeWillLearn} by building TaskOPedia!{" "}
+        <br />
+        Total Lecture - {lectureCount}
+      </p>
       <ul>
-        <li>Call Ben</li>
-        <li>Go to walmart</li>
+        <li>Basic Foundation</li>
+        <li>Functional and Class Components</li>
       </ul>
+      {/* <div>
+        Enter Task : <input maxLength={5} readOnly={false} placeholder="Ben"></input>
+      </div> */}
     </div>
   );
 }
+
 function Footer() {
   return (
     <p style={{ color: "gray", backgroundColor: "black" }}>Happy Coding!</p>
   );
 }
 root.render(
-  <div>
+  <div className="container">
     <Header />
     <MainBody />
+    <div className="row">Students Enrolled</div>
+    <Student experience={2} name="Cris Pratt"/>
+    <Student experience={5} name="Patric Stuard"/>
+    <Student experience={3} name="Cris Evans"/>
     <Footer />
   </div>
 );
